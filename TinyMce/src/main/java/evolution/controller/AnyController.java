c5a3103562f3548d6974813d055cd7fe9c2857c4
chenli <chenli@chens-air>
2017-07-04 09:42:45 +0800

    refactor

--- TinyMce/src/main/java/evolution/controller/AnyController.java
+++ TinyMce/src/main/java/evolution/controller/AnyController.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AnyController {
 	@PostMapping("/post")
-	public void post(@RequestParam("html") String html) {
+	public String post(@RequestParam("html") String html) {
 		System.out.println(html);
+		return html;
 	}
 }
